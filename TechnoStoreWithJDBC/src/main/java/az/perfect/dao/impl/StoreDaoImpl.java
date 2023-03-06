de3bd25fa666e67f18c97ed7ae0770842a8ea004
shahbzly <shahbzly@gmail.com>
2023-03-06 13:24:48 +0400

    Update StoreDaoImpl.java

--- TechnoStoreWithJDBC/src/main/java/az/perfect/dao/impl/StoreDaoImpl.java
+++ TechnoStoreWithJDBC/src/main/java/az/perfect/dao/impl/StoreDaoImpl.java
@@ -10,9 +10,11 @@ import az.perfect.entity.Product;
 import az.perfect.entity.Store;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,31 +24,89 @@ import java.util.List;
 public class StoreDaoImpl implements StoreDaoInter {
 
     public StoreDaoImpl() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     @Override
     public void insert(Store s) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try ( Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/technostore",
+                "boot1", "1234")) {
+
+            PreparedStatement st = conn.prepareStatement("insert into store values(?,?,?,?)");
+            st.setInt(1, s.getId());
+            st.setString(2, s.getStore_name());
+            st.setString(3, s.getLocation());
+            st.setString(4, s.getPhone());
+            
+
+            st.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void update(Store s) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try ( Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/technostore",
+                "boot1", "1234")) {
+
+            PreparedStatement st = conn.prepareStatement("update store set store_name=?,location=?,phone=? where id=?");
+            st.setString(1, s.getStore_name());
+            st.setString(2, s.getLocation());
+            st.setString(3, s.getPhone());
+            st.setInt(4, s.getId());
+
+            st.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
-    public void delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void delete(int id
+    ) {
+        try ( Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/technostore",
+                "boot1", "1234")) {
+
+            Statement st = conn.createStatement();
+            st.execute("delete from store where id=" + id);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public List<Store> getAllStores() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Store> list = new ArrayList();
+
+        try ( Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/technostore",
+                "boot1", "1234")) {
+
+            Statement st = conn.createStatement();
+
+            ResultSet rs = st.executeQuery("SELECT s.* from store s");
+
+            while (rs.next()) {
+                int sid = rs.getInt("id");
+                String store_name = rs.getNString("store_name");
+                String location = rs.getNString("location");
+                String phone = rs.getNString("phone");
+                Store s = new Store(sid, store_name, location, phone);
+                list.add(s);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
     }
 
     @Override
-    public Store getStoreById(int id) {
+    public Store getStoreById(int id
+    ) {
         Store s = null;
         try ( Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/technostore",
                 "boot1", "1234")) {
@@ -60,7 +120,7 @@ public class StoreDaoImpl implements StoreDaoInter {
                 String store_name = rs.getNString("store_name");
                 String location = rs.getNString("location");
                 String phone = rs.getNString("phone");
-                s = new Store (sid,store_name,location,phone);
+                s = new Store(sid, store_name, location, phone);
 
             }
 
